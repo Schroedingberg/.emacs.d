706a8a660e91161e2ccfd8390ca7e4fd2b5be0f8
Schroedingberg <aaronrebmann@gmail.com>
2016-03-18 12:15:31 +1100

    Init as org file added. Cleaning of the directory necessary

--- elpa/dumb-jump-20160314.2102/dumb-jump-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "dumb-jump" "20160314.2102" "jump to variable and function definitions with no configuration" '((f "0.17.3") (s "1.11.0") (dash "2.9.0") (popup "0.5.3")) :keywords '("programming"))
